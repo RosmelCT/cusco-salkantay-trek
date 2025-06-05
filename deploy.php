ce36a2a9fc104b8eed8816dd77358c0bfdde4b7e
“RosmelTC” <rosmel.condetravel@gmail.com>
2025-06-04 20:02:13 -0400

    Primer commit desde cPanel

--- deploy.php
+++ deploy.php
@@ -1,15 +1,10 @@
 <?php
-// deploy.php - ejecuta git pull al recibir webhook de GitHub
+// deploy.php - se ejecuta al recibir el webhook
 $log = "deploy.log";
-
-// Escribe en el archivo log la hora en que se ejecutó
 file_put_contents($log, "Webhook recibido a las " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
 
-// Ejecuta git pull con la clave SSH explícita
 $salida = [];
-exec('cd /home/salkantay/cuscosalkantaytrek.com && GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no" git pull origin main 2>&1', $salida);
-
-// Guarda la salida del comando en el log
+exec('cd /home/salkantay/cuscosalkantaytrek.com && git pull origin main 2>&1', $salida);
 file_put_contents($log, implode("\n", $salida) . "\n", FILE_APPEND);
 ?>
 
